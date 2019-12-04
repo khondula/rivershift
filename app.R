fbeb1d6e18b26c276584eb6f187616b06fee5503
Kelly Hondula <khondula@rstudio03.research.sesync.org>
2019-12-04 14:02:12 -0500

    update paths

--- app.R
+++ app.R
@@ -8,20 +8,24 @@
 #
 
 library(shiny)
-library(plotly)
 
+data_dir <- "/nfs/rivershift-data"
+sites_all3_streams <- readr::read_csv("/nfs/rivershift-data/us-data/sites_all3_streams.csv", col_types = "c")
+
+    
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
     titlePanel("NWIS data for sites with all 3 parameters"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar  
     sidebarLayout(
         sidebarPanel(
             selectInput("site_no",
                         "Site ID:",
-                        choices = sites_all3), width = 2
+                        choices = sites_all3_streams),
+            width = 2
         ),
 
         # Show a plot of the generated distribution
@@ -29,22 +33,23 @@ ui <- fluidPage(
             tabsetPanel(
                 tabPanel("summary", tableOutput("summary_table")),
                 tabPanel("data", tableOutput("site_data_df")),
-                tabPanel("plots", plotOutput("ts_plots", height = "2000px")))
+                tabPanel("plots", plotOutput("ts_plots", height = "2000px")),
+                tabPanel("site", tableOutput("site_info")))
             )
         )
     )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
 
     output$summary_table <- renderTable({
-        readr::read_csv(file = glue::glue("data/nwis_site_summaries/nwis-summary_{input$site_no}.csv"),
+        readr::read_csv(file = glue::glue("{data_dir}/us-data/nwis_site_summaries/nwis-summary_{input$site_no}.csv"),
                         col_types = c("ciiincccccc")) %>%
             dplyr::select(srsname, parameter_nm, parameter_units, date_range_yrs, n_count, min_year, max_year)
     })
     
     site_data <- reactive({
-        readr::read_csv(file = glue::glue("data/nwis_site_data/nwis-data_{input$site_no}.csv"),
+        readr::read_csv(file = glue::glue("{data_dir}/us-data/nwis_site_data/nwis-data_{input$site_no}.csv"),
                         col_types = cols_only(
                             agency_cd = col_character(),
                             site_no = col_character(),
@@ -61,7 +66,7 @@ server <- function(input, output) {
     output$site_data_df <- renderTable({
         # head(site_data())
         # site_data() %>% mutate(startDateTime = as.POSIXct(startDateTime))
-        readr::read_csv(file = glue::glue("data/nwis_site_data/nwis-data_{input$site_no}.csv"),
+        readr::read_csv(file = glue::glue("{data_dir}/us-data/nwis_site_data/nwis-data_{input$site_no}.csv"),
                         col_types = cols_only(
                             agency_cd = col_character(),
                             site_no = col_character(),
@@ -86,6 +91,11 @@ server <- function(input, output) {
             geom_point(aes(fill = category), pch = 21, size = 2) +
             facet_wrap(vars(parameter_nm), scales = "free_y", ncol = 1) +
             theme(legend.position = "none")
+    })
+    
+    output$site_info <- renderTable({
+        dataRetrieval::readNWISsite(input$site_no) %>% 
+            t() %>% as.data.frame() %>% tibble::rownames_to_column()
     })
 }
 
